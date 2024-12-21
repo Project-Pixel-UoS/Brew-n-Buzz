62b4abf9ca7b0e5c21c93ef0a07c1a79762ce46f
lilyxburke <94839155+lilyxburke@users.noreply.github.com>
2024-12-21 14:34:34 +0000

    [refactor]:Changing the GameManager to a singleton

--- scenes/money_counter.gd
+++ scenes/money_counter.gd
@@ -1,8 +1,7 @@
 extends Control
 
 @onready var money_label: Label = $"."
-@onready var game_manager: Node = $"../../../../GameManager"
 
 func _process(delta: float) -> void:
-	money_label.text=str(game_manager.get_money())
+	money_label.text=str(GameManager.get_money())
 	
