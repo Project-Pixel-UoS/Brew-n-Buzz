567028be32d10ee3d581ba60cbc13164bdcbe1be
Olivia <104592012+OliviaStarStuff@users.noreply.github.com>
2024-12-22 13:46:43 +0000

    [docs]: Update comments on doll.gd

--- prototype/doll.gd
+++ prototype/doll.gd
@@ -2,17 +2,16 @@ extends Node2D
 
 @export var character: Customer
 
-# Called when the node enters the scene tree for the first time.
+## @brief sets up the customer
 func _ready() -> void:
-	print(character.body.head)
 	$head.texture = character.body.head
 	$body.texture = character.body.body
 	$face.texture = character.body.face
 	$hair.texture = character.body.hair
-	#$head._on_button_pressed()
-	#$body._on_button_pressed()
-	#$face._on_button_pressed()
-	#$hair._on_button_pressed()
 
 func _on_button_pressed() -> void:
-	_ready()
+	# randomly assigns texture
+	$head._on_button_pressed()
+	$body._on_button_pressed()
+	$face._on_button_pressed()
+	$hair._on_button_pressed()
