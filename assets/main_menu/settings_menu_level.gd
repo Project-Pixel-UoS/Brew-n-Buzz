5ac73ffe11c6c509b3e7b12eb7ef29578647a44c
lilyxburke <94839155+lilyxburke@users.noreply.github.com>
2025-02-21 19:31:20 +0000

    [wip]: Settings page opens and closes in level

---
+++ assets/main_menu/settings_menu_level.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var settingsMenu = %settings_menu
+@onready var settingsLayer = %SettingsLayer
+
+func _ready():
+	var button = settingsMenu.get_node("BackButton")
+	button.connect("pressed", Callable(self, "_on_button_pressed"))
+
+func _on_button_pressed():
+	settingsLayer.visible = false
