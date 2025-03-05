ba476b010c869f16b8a208a4a1fde825a67da52d
lilyxburke <94839155+lilyxburke@users.noreply.github.com>
2025-03-05 22:29:33 +0000

    [wip]: Grinder keeps count of amount of coffee inside

--- scripts/Machines/grinder.gd
+++ scripts/Machines/grinder.gd
@@ -1,30 +1,23 @@
 extends Node
 
-var max_amount = 8 # potential to have a set max_amount function later
-var amount_left # amount left in grinder
+var max_amount = 5 
+var amount_in 
 var coffee_grinded = true
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	amount_left = randi() % max_amount + 1
-	print(amount_left , " and " ,max_amount)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	pass ## need to code how to catch teh object when falls into script, idk how :( plz help fanks
+	amount_in = 0
 
 
 func fill_grinder() -> void:
-	if amount_left == max_amount:
+	amount_in += 1
+	print(amount_in)
+	#run animation here
+	if amount_in >= max_amount:
 		print("Grinder is already full!")
-	amount_left = max_amount
-	print(amount_left)
+	#auto grinding???
 	coffee_grinded = true
 	
-
-func get_amount() -> int: #will be needed for grinder visual level amount on sprite.
-	return amount_left
+func get_amount() -> int:
+	return amount_in
 
 func is_coffee_grinded() -> bool:
 	return coffee_grinded
