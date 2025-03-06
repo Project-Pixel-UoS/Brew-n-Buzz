2d8a9a483bfe7abbdc5fb1f375b76ae048825516
lilyxburke <94839155+lilyxburke@users.noreply.github.com>
2025-03-06 07:32:14 +0000

    [wip] Grinder sprites change for addition and removal of coffee

--- scripts/Machines/grinder.gd
+++ scripts/Machines/grinder.gd
@@ -2,19 +2,38 @@ extends Node
 
 var max_amount = 5 
 var amount_in 
-var coffee_grinded = true
+var coffee_grinded = false
+var path = "res://images/kitchen panel/machines/grinder sprites/"
+var dir_access = DirAccess.open(path)
+var grinder_sprites = []
+
 func _ready() -> void:
 	amount_in = 0
+	var files = dir_access.get_files()
+	for file in files:
+		if file.ends_with(".import"):
+			continue
+		else:
+			var loaded_resource = load(path + file)
+			grinder_sprites.append(loaded_resource)
+	get_node("Sprite2D").texture = grinder_sprites[0]
 
+func remove_coffee():
+	get_node("Sprite2D").texture = grinder_sprites[amount_in-1]
+
+func add_coffee():
+	get_node("Sprite2D").texture = grinder_sprites[amount_in]
 
 func fill_grinder() -> void:
 	amount_in += 1
 	print(amount_in)
 	#run animation here
-	if amount_in >= max_amount:
+	if amount_in > max_amount:
 		print("Grinder is already full!")
+	else:
+		add_coffee()
+		coffee_grinded = true
 	#auto grinding???
-	coffee_grinded = true
 	
 func get_amount() -> int:
 	return amount_in
