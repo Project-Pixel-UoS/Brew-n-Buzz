9e2a1ae27a7d359e018f42a859718d48d98d148d
lilyxburke <94839155+lilyxburke@users.noreply.github.com>
2025-03-06 22:50:13 +0000

    [feat]: Add sprites and audio for guidebook

--- scenes/guidebook_menu.gd
+++ scenes/guidebook_menu.gd
@@ -3,23 +3,34 @@ extends Control
 @onready var guidebook_menu = %guidebook_menu
 var pages = []
 var current_page_index = 0
-# Called when the node enters the scene tree for the first time.
+@export var page_sounds = []
+var path = "res://audio/sfx/guidebook sounds/page sounds/"
+var dir_access = DirAccess.open(path)
+var rng = RandomNumberGenerator.new()
+
 func _ready() -> void:
-	var button = guidebook_menu.get_node("Button")
+	var files = dir_access.get_files()
+	for file in files:
+		if file.ends_with(".import"):
+			continue
+		else:
+			var loaded_resource = load(path + file)
+			page_sounds.append(loaded_resource)
+	var button = guidebook_menu.get_node("TextureButton")
 	button.connect("pressed", Callable(self, "_on_button_pressed"))	
 	pages = get_node("Pages").get_children()
 	for child in pages:
 		child.visible = false
-	print(pages)
+	
 func _on_button_pressed(): #back button
+	AudioManager.set_stream(load('res://audio/sfx/guidebook sounds/book_close.wav'))
+	AudioManager.play()
 	guidebook_layer.visible = false
 	
 func get_current_page_index():
 	return current_page_index
 	
 func show_correct_page(number):
-	print(number)
-	print(current_page_index)
 	pages[current_page_index].visible = false
 	pages[number].visible = true
 	current_page_index = number
@@ -32,10 +43,19 @@ func show_correct_page(number):
 	else:
 		guidebook_menu.get_node("RightButton").visible = true
 		guidebook_menu.get_node("LeftButton").visible = true
+		
 func get_next_right_page():
+	var number = rng.randi_range(0,page_sounds.size()-1)
+	AudioManager.set_stream(page_sounds[number])
+	AudioManager.play()
+	await get_tree().create_timer(0.3).timeout
 	show_correct_page(get_next_page("right"))
 	
 func get_next_left_page():
+	var number = rng.randi_range(0,page_sounds.size()-1)
+	AudioManager.set_stream(page_sounds[number])
+	AudioManager.play()
+	await get_tree().create_timer(0.3).timeout
 	show_correct_page(get_next_page("left"))
 	
 func get_next_page(value) -> int:
