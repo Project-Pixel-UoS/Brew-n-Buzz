db4528e9d45326b547f677fabc16994f77c63f36
Tom Foster <trfoster1794@gmail.com>
2025-03-13 04:07:59 +0000

    [feat]: integrated customer panel into level 1

--- prototype/doll.gd
+++ prototype/doll.gd
@@ -3,11 +3,11 @@ extends Node2D
 @export var character: Customer
 
 ## @brief sets up the customer
-func _ready() -> void:
-	$head.texture = character.body.head
-	$body.texture = character.body.body
-	$face.texture = character.body.face
-	$hair.texture = character.body.hair
+#func _ready() -> void:
+	#$head.texture = character.body.head
+	#$body.texture = character.body.body
+	#$face.texture = character.body.face
+	#$hair.texture = character.body.hair
 
 func _on_button_pressed() -> void:
 	# randomly assigns texture
