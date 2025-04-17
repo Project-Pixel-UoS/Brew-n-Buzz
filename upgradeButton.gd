0bc77dee3298162e664b9f7fc540431eadd18877
lilyxburke <lburke701@gmail.com>
2025-04-17 12:12:35 +0100

    [feat][fix]: Price button changes price after purchasing and the correct amount is deducted from a player's money

--- upgradeButton.gd
+++ upgradeButton.gd
@@ -1,7 +1,11 @@
 extends TextureButton
 
 var machineName = name.split("Button")[0]
-
+var current_price = 0
 func _on_pressed() -> void:
-	GameManager.remove_money(10)
+	print(current_price)
+	GameManager.remove_money(current_price)
 	GameManager.add_upgrade(machineName)
+	
+func set_price(price):
+	current_price = price
--- upgrade_amount.gd
+++ upgrade_amount.gd
@@ -8,12 +8,12 @@ var upgrades = 0
 func _ready() -> void:
 	var machine_name = name.split('_')[0]
 	machine = levelManager.get_node('Machines/' + machine_name)
-	print(machine.name)
 	buyButton = parent_node.get_node('Buy')
-	upgrades = GameManager.get_number_upgrades(machine_name)
 		
 func _process(delta: float) -> void:
+	upgrades = GameManager.get_number_upgrades(machine.name)
 	var beginning_time = machine.get_current_time()
 
 	self.text = '[center]' + str(beginning_time) + " -> " + str(beginning_time-0.25)
 	buyButton.get_node('Price').text = '[center]' + str(10 + (2*upgrades))
+	buyButton.set_price(10+(2*upgrades))
