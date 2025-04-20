f83e2b079b5c82a042c10f96f91e67ee7da7fd7e
lilyxburke <lburke701@gmail.com>
2025-04-20 10:26:16 +0100

    [feat]: Upgrades are now seeded

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -3,9 +3,18 @@ extends Node
 @onready var levelManager: Node2D = get_tree().root.get_node("Level1")
 var total_money = 0
 var upgrades = [['Grinder', 0]]
+var shop_upgrade_dictionary = {'A': "Grinder", 'B': "CoffeeMachine"}
+var level_upgrade_dictionary = {1: ['A']}
+
 ## @brief for when an object is in a 'dragging state'
 var is_dragging = false
 
+func get_level_upgrades(level_number):
+	return level_upgrade_dictionary[level_number]
+	
+func get_upgrade_name_from_key(key):
+	return shop_upgrade_dictionary[key]
+	
 func add_money(money):
 	total_money += money
 	
