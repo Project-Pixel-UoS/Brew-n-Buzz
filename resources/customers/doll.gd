6cdf21963da3a72fc71f90e898d8dc92cb2a8597
lilyxburke <94839155+lilyxburke@users.noreply.github.com>
2025-05-03 17:21:24 +0100

    [feat]: NPC's now have an animation

--- resources/customers/doll.gd
+++ resources/customers/doll.gd
@@ -1,20 +1,27 @@
 extends Node2D
 
 @export var customer: CustomerData
+var old_position 
+var new_position
+var finished_animation = false
 
 func set_customer():
 	update_customer_appearance()
 	say_dialogue()
 	if customer.idle_body_texture == null:
-		pass
-		#do NPC lerping animation
+		new_position = Vector2(old_position.x, old_position.y -2)
+		while not finished_animation:
+			await NPC_animation()
 	else:
 		%AnimationPlayer.play('special')
-	
+func NPC_animation():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", new_position, 1).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "position", old_position, 0.9).set_ease(Tween.EASE_OUT)
+	await tween.finished
 func get_customer() -> CustomerData:
 	return customer
-	
-	
+
 func update_customer_appearance():
 	if customer == null:
 		push_warning("Customer is null in Doll.gd!")
@@ -24,6 +31,7 @@ func update_customer_appearance():
 	$face.texture = customer.face_texture
 	$hair.texture = customer.hair_texture
 	$full_body.texture = customer.special_body_texture
+	old_position = self.position
 	
 func say_dialogue():
 	# format dialogue
@@ -42,6 +50,7 @@ func react_to_drink(correct: bool):
 		%AnimationPlayer.play("happy")
 	else:
 		%AnimationPlayer.play("angry")
+	finished_animation = true
 		
 func reset_sprites():
 	$head.texture = null
