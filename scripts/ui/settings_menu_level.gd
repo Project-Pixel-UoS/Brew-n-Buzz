8ffcd64fcf9cfca83a9a7393536179438f759e01
lilyxburke <lburke701@gmail.com>
2025-05-12 22:30:42 +0100

    [chore]: Updating settings page in game

--- scripts/ui/settings_menu_level.gd
+++ scripts/ui/settings_menu_level.gd
@@ -2,11 +2,13 @@ extends Control
 
 @onready var settingsMenu = %settings_menu
 @onready var settingsLayer = %SettingsLayer
+var button
 
 func _ready():
-	var button = settingsMenu.get_node("BackButton")
+	button = settingsMenu.get_node("BackButton")
 	button.connect("pressed", Callable(self, "_on_button_pressed"))
 
 func _on_button_pressed():
 	settingsLayer.visible = false
 	Engine.time_scale = 1
+	
