1cfb4345b3a091b556bde139246b091aaa53fbc6
lilyxburke <lburke701@gmail.com>
2025-05-13 21:00:20 +0100

    [chore]: Updated level icon size

--- scripts/general/level_button.gd
+++ scripts/general/level_button.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 	name = "LevelButton%d" % level_number
 	level_filename = "scenes/levels/level_%d.tscn" % level_number
 	self.texture_normal = load("res://assets/images/menu/level icons/level_%d_icon.png" % level_number)
-	self.custom_minimum_size = Vector2(128, 128)
+	self.custom_minimum_size = Vector2(192,192)
 ## @brief Loads levelscne
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file(level_filename)
