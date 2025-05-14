9f1f85e703434de1575a70e8d6b060d3078653e3
Tom Foster <trfoster1794@gmail.com>
2025-05-14 15:07:41 +0100

    [feat]: added pause menu

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -2,22 +2,19 @@ extends Control
 @onready var pauseLayer = %PauseLayer
 @onready var pause_menu = %pause_menu
 @onready var levelManager = get_tree().root.get_node("Level1")
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var button = pause_menu.get_node("TextureButton")
-	button.connect("pressed", Callable(self, "_on_button_pressed"))	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _on_back_button_pressed():
 	pauseLayer.visible = false
 	Engine.time_scale = 1
 
-func exit_without_saving_pressed():
+func _on_exit_level_button_pressed() -> void:
 	Engine.time_scale = 1
-	get_tree().change_scene_to_file("scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("scenes/main_menu/main_menu.tscn")
 	
-func exit_with_saving_pressed():
-	levelManager.save_level()
-	Engine.time_scale = 1
-	print("saving level...")
-	get_tree().change_scene_to_file("scenes/main_menu.tscn")
+
+#func exit_with_saving_pressed():
+	#levelManager.save_level()
+	#Engine.time_scale = 1
+	#print("saving level...")
+	#get_tree().change_scene_to_file("scenes/main_menu/main_menu.tscn")
