3a3b44fbcea8c21c456a58bfa8783a555bc80774
Olivia <104592012+OliviaStarStuff@users.noreply.github.com>
2025-07-01 18:55:49 +0100

    [clean]: remove unused recipe tier resource

--- resources/drinks/recipe_tiers/recipe_tier.gd
+++
@@ -1,9 +0,0 @@
-class_name RecipeTier extends Resource
-
-@export var ingredients: Array[Ingredient]
-
-func size():
-	return ingredients.size()
-
-func has(ingredient):
-	return ingredients.has(ingredient)
